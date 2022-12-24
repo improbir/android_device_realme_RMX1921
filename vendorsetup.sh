6739860773d2b9aa4f45b1757b5927ee76021638
improbir <ghosh_probir@yahoo.com>
2022-12-24 18:46:38 +0000

    RMX1921: Init vendorsetup
    Signed-off-by: improbir <ghosh_probir@yahoo.com>

---
+++ vendorsetup.sh
@@ -0,0 +1,5 @@
+#Kernel Tree
+git clone https://github.com/improbir/kernel_realme_sdm710.git -b 13 kernel/realme/sdm710
+
+#Vendor Tree
+git clone https://gitlab.com/improbir/vendor_realme_RMX1921.git -b 13 vendor/realme/RMX1921 
