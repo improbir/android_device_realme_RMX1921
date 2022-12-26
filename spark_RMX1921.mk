bc764071f1c9e5ca1caa24bc714eb3d5dd753c37
improbir <ghosh_probir@yahoo.com>
2022-12-26 06:07:10 +0000

    RMX1921: Init spark os
    Signed-off-by: improbir <ghosh_probir@yahoo.com>

--- spark_RMX1921.mk
+++ spark_RMX1921.mk
@@ -4,11 +4,13 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit some common Evolution X stuff
+# Inherit some common Spark stuff
 TARGET_BOOT_ANIMATION_RES := 1080
 EXTRA_UDFPS_ANIMATIONS := true
 TARGET_SUPPORTS_QUICK_TAP := true
-$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
+WITH_GAPPS := true
+TARGET_GAPPS_ARCH := arm64
+$(call inherit-product, vendor/spark/config/common_full_phone.mk)
 
 # Inherit from RMX1921 device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
@@ -16,7 +18,7 @@ $(call inherit-product, $(LOCAL_PATH)/device.mk)
 PRODUCT_BRAND := realme
 PRODUCT_DEVICE := RMX1921
 PRODUCT_MANUFACTURER := realme
-PRODUCT_NAME := evolution_RMX1921
+PRODUCT_NAME := spark_RMX1921
 PRODUCT_MODEL := RMX1921
 
 PRODUCT_SYSTEM_NAME := RMX1921
