354e141bd4bf706a3f899fd30702b6747c1ff00d
improbir <ghosh_probir@yahoo.com>
2022-12-26 14:26:17 +0000

    RMX1921: Track hals from lineage
    Signed-off-by: improbir <ghosh_probir@yahoo.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -3,3 +3,15 @@ git clone https://github.com/improbir/kernel_realme_sdm710.git -b 13 kernel/real
 
 #Vendor Tree
 git clone https://github.com/improbir/vendor_realme_RMX1921.git -b 13 vendor/realme/RMX1921 
+
+#Clean existing hals
+rm -rf hardware/qcom-caf/sdm845
+
+#Audio hal
+git clone https://github.com/LineageOS/android_hardware_qcom_audio.git -b lineage-20.0-caf-sdm845 hardware/qcom-caf/sdm845/audio
+
+#Display hal
+git clone https://github.com/LineageOS/android_hardware_qcom_display.git -b lineage-20.0-caf-sdm845 hardware/qcom-caf/sdm845/display
+
+#Media hal
+git clone https://github.com/LineageOS/android_hardware_qcom_media.git -b lineage-20.0-caf-sdm845 hardware/qcom-caf/sdm845/media
