60a94f6fa35e4a4b3e663b005d49efc1a638f5b6
improbir <ghosh_probir@yahoo.com>
2023-06-18 02:01:54 +0000

    RMX1921: Init PE
    Signed-off-by: improbir <ghosh_probir@yahoo.com>

--- aosp_RMX1921.mk
+++ aosp_RMX1921.mk
@@ -4,14 +4,13 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit some common DerpFest stuff
+# Inherit some common PE stuff
 TARGET_BOOT_ANIMATION_RES := 1080
-EXTRA_UDFPS_ANIMATIONS := true
-TARGET_NOT_USES_BLUR := true
-$(call inherit-product, vendor/derp/config/common_full_phone.mk)
-
-# Official-ify
-DERP_BUILDTYPE := Official
+TARGET_SUPPORTS_QUICK_TAP := true
+WITH_GAPPS := true
+TARGET_GAPPS_ARCH := arm64
+TARGET_FACE_UNLOCK_SUPPORTED := true
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Inherit from RMX1921 device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
@@ -19,7 +18,7 @@ $(call inherit-product, $(LOCAL_PATH)/device.mk)
 PRODUCT_BRAND := realme
 PRODUCT_DEVICE := RMX1921
 PRODUCT_MANUFACTURER := realme
-PRODUCT_NAME := derp_RMX1921
+PRODUCT_NAME := aosp_RMX1921
 PRODUCT_MODEL := RMX1921
 
 PRODUCT_SYSTEM_NAME := RMX1921
