20bdc24f76b4b59d5500ac2ef4a6cf0850727f3f
improbir <ghosh_probir@yahoo.com>
2023-06-27 05:14:21 +0000

    RMX1921: Track display hal from PE
    Signed-off-by: improbir <ghosh_probir@yahoo.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -11,7 +11,7 @@ rm -rf hardware/qcom-caf/sdm845
 git clone https://github.com/LineageOS/android_hardware_qcom_audio.git -b lineage-20.0-caf-sdm845 hardware/qcom-caf/sdm845/audio
 
 #Display hal
-git clone https://github.com/LineageOS/android_hardware_qcom_display.git -b lineage-20.0-caf-sdm845 hardware/qcom-caf/sdm845/display
+git clone https://github.com/PixelExperience/hardware_qcom-caf_sdm845_display.git -b thirteen hardware/qcom-caf/sdm845/display
 
 #Media hal
 git clone https://github.com/LineageOS/android_hardware_qcom_media.git -b lineage-20.0-caf-sdm845 hardware/qcom-caf/sdm845/media
