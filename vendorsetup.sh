130f76e690c54ee5d8d22727381b8a6f794e0401
improbir <ghosh_probir@yahoo.com>
2023-07-19 15:45:08 +0000

    fix link
    Signed-off-by: improbir <ghosh_probir@yahoo.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,7 +2,7 @@
 git clone https://github.com/improbir/kernel_realme_sdm710.git -b 13 kernel/realme/sdm710
 
 #Vendor Tree
-git clone https://gitlab.com/improbir/vendor_realme_RMX1921.git -b 13 vendor/realme/RMX1921 
+git clone https://github.com/improbir/android_vendor_realme_RMX1921.git -b 13 vendor/realme/RMX1921 
 
 #Clean existing hals
 rm -rf hardware/qcom-caf/sdm845
