4f0d6dee9baeb3c3b537a876aec6e70643040402
Master <mhmdeveloperofficial@gmail.com>
2023-11-03 04:49:21 +0000

    RMX1921: Add ViPER4Android FX support
    * Need to clone this:
    git clone https://github.com/swiitch-OFF-Lab/packages_apps_ViPER4AndroidFX packages/apps/ViPER4AndroidFX
    
    Signed-off-by: kaderbava <ksbava7325@gmail.com>
    Signed-off-by: improbir <ghosh_probir@yahoo.com>

--- aosp_RMX1921.mk
+++ aosp_RMX1921.mk
@@ -15,6 +15,9 @@ $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 # Inherit from RMX1921 device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
+# Viper
+$(call inherit-product, packages/apps/ViPER4AndroidFX/config.mk)
+
 PRODUCT_BRAND := realme
 PRODUCT_DEVICE := RMX1921
 PRODUCT_MANUFACTURER := realme
