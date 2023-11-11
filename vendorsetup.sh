ecc881bf5b3ea15811b4c4f5b7e6219cfcfd6069
improbir <ghosh_probir@yahoo.com>
2023-11-11 19:04:23 +0000

    RMX1921: Move to Android 14 branch
    Signed-off-by: improbir <ghosh_probir@yahoo.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -8,13 +8,13 @@ git clone https://github.com/improbir/android_vendor_realme_RMX1921.git -b 13 ve
 rm -rf hardware/qcom-caf/sdm845
 
 #Audio hal
-git clone https://github.com/LineageOS/android_hardware_qcom_audio.git -b lineage-20.0-caf-sdm845 hardware/qcom-caf/sdm845/audio
+git clone https://github.com/LineageOS/android_hardware_qcom_audio.git -b lineage-21.0-caf-sdm845 hardware/qcom-caf/sdm845/audio
 
 #Display hal
-git clone https://github.com/LineageOS/android_hardware_qcom_display.git -b lineage-20.0-caf-sdm845 hardware/qcom-caf/sdm845/display
+git clone https://github.com/LineageOS/android_hardware_qcom_display.git -b lineage-21.0-caf-sdm845 hardware/qcom-caf/sdm845/display
 
 #Media hal
-git clone https://github.com/LineageOS/android_hardware_qcom_media.git -b lineage-20.0-caf-sdm845 hardware/qcom-caf/sdm845/media
+git clone https://github.com/LineageOS/android_hardware_qcom_media.git -b lineage-21.0-caf-sdm845 hardware/qcom-caf/sdm845/media
 
 #Clang-r416183b1
 git clone --depth=1 https://github.com/ArrowOS-Devices/android_prebuilts_clang_host_linux-x86_clang-r416183b1  -b arrow-11.0 prebuilts/clang/host/linux-x86/clang-r416183b1
