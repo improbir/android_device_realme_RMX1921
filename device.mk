0a51da0437cd0c02fd2377dbbd97a691d6c1efec
improbir <ghosh_probir@yahoo.com>
2024-03-22 06:04:12 +0000

    RMX1921: Build vendor.display.config@1.9
    Signed-off-by: improbir <ghosh_probir@yahoo.com>

--- device.mk
+++ device.mk
@@ -141,6 +141,8 @@ PRODUCT_PACKAGES += \
     libqdMetaData \
     libqdMetaData.system \
     vendor.display.config@1.0.vendor \
+    vendor.display.config@1.9 \
+    vendor.display.config@1.9.vendor \
     vendor.display.config@2.0 \
     vendor.display.config@2.0.vendor \
     vendor.qti.hardware.display.allocator@1.0-service \
